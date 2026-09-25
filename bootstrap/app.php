a9c0bd747cbb971af319d9fc0761a63f902c6c7d
anantawijaaya <anantawijaya@example.com>
2026-09-25 09:31:32 +0800

    eror fix tombol import part.2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'admin.only' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
